2ec804c89ce9da6f0c4eea137e07c88ba958098e
Mahmoud Bassam <mahmoudbassam1999@gmail.com>
2024-07-28 18:35:14 +0300

    USeles_file will removed later

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void welcome_message(void);
+
+int main(int argc, char** argv) {
+	welcome_message();
+	return 0;
+}
+void welcome_message(void){
+	std::cout<<"Hello_world\n";
+}
